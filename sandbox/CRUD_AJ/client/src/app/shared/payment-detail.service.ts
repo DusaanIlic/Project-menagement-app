bd7105ebcd0248bf596a333f208372b1b4306c66
aleksJankovic <saska.jankovic19@gmail.com>
2024-02-28 23:16:15 +0100

    povezani bek i front, prikaz liste korisnika iz baze #8

---
+++ sandbox/CRUD_AJ/client/src/app/shared/payment-detail.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { error } from 'console';
+import { PaymentDetail } from './payment-detail.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  url:string = environment.apiBaseUrl+'/PaymentDetail'
+  list:PaymentDetail[]=[];
+  constructor(private http:HttpClient) { }
+
+  refreshList(){
+    this.http.get(this.url)
+    .subscribe({
+      next : res => {
+        this.list = res as PaymentDetail[]},
+      error : err => {console.log(err)}
+    })
+  }
+}
--- sandbox/CRUD_AJ/server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ sandbox/CRUD_AJ/server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a95d45f511b8cedeeb51a80e1475450a04aa9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab63d3c7474681a3833f7a64f626a8100c5fc24")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
