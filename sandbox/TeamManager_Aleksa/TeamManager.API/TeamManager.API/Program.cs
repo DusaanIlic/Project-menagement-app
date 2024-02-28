fbf9751b8c8acb48688b74f1066607dac2a26495
--set <--set>
2024-02-29 00:03:42 +0100

    [#7] Promenjen naziv crud projekta, inicijalizovan angular deo

---
+++ sandbox/TeamManager_Aleksa/TeamManager.API/TeamManager.API/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using TeamManager.API.Models;
+
+namespace TeamManager.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<TeamManagerContext>(opt =>
+            opt.UseInMemoryDatabase("TeamManager"));
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        
+        app.Run();
+    }
+}
