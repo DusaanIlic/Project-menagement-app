54323da602c9bc41f34bb06ee18f02dadb57c0c4
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-02-29 01:46:04 +0100

    #10 Dodata veza izmedju backend i frontend dela.

--- sandbox/BlogApp_Veljko/blogapp_veljko.client/src/app/app.module.ts
+++ sandbox/BlogApp_Veljko/blogapp_veljko.client/src/app/app.module.ts
@@ -1,19 +1,30 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CategoryListComponent } from './features/categories/category-list/category-list.component';
+import { AddCategoryComponent } from './features/categories/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryListComponent,
+    AddCategoryComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
