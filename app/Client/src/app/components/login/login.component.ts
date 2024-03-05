78c5b919cfa3c44d04e2de9cc8bd14a9d1c261fd
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-05 18:46:28 +0100

    [#20] Završena osnovna login komponenta, čeka se bek za ostatak

--- app/Client/src/app/components/login/login.component.ts
+++ app/Client/src/app/components/login/login.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import {NgIf, NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   errorMessage: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+
+  onSubmit(): void {
+    if (!this.email || !this.password) {
+      this.errorMessage = "* Email and password are required.";
+      return;
+    }
+
+    console.log(this.email, this.password);
+  }
+
+  /* Cekam bek za dalje */
 }
