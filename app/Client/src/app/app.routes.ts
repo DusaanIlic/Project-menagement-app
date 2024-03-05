1ae09851dff9664fd78269699b328b6b99d45a63
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-03-05 22:16:12 +0100

    #23 Dodate komponente u app-route i izmene u app.component fajlu

--- app/Client/src/app/app.routes.ts
+++ app/Client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AllMembersComponentComponent } from './components/all-members-component/all-members-component.component';
+import { AllProjectsComponentComponent } from './components/all-projects-component/all-projects-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "memebrs/allMembers", component: AllMembersComponentComponent},
+    {path: "projects/allProjects", component: AllProjectsComponentComponent}
+];
