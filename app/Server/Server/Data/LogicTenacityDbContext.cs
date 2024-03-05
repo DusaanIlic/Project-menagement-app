8c69c77b7b8a04388068bed394883b3adc6cf47c
aleksJankovic <saska.jankovic19@gmail.com>
2024-03-05 14:35:10 +0100

    #25 dodat model, povezano sa bazom

---
+++ app/Server/Server/Data/LogicTenacityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Data
+{
+    public class LogicTenacityDbContext : DbContext
+    {
+        public LogicTenacityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public  DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ app/Server/Server/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+    
+        public string ProjectName { get; set; } = string.Empty;
+
+        public string ProjectDescription { get; set; } = string.Empty;
+    }
+}
