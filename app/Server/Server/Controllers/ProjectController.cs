180dab8f4bdc3497bfc4b3a8a87441272b06c2e3
aleksJankovic <saska.jankovic19@gmail.com>
2024-03-05 14:47:06 +0100

    #25 obrisani nepotrebni fajlovi, dodat ProjectController

---
+++ app/Server/Server/Controllers/ProjectController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly LogicTenacityDbContext _context;
+
+        public ProjectController(LogicTenacityDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
