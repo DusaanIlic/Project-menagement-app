418afb543ec8dd9bed7bffbe350c811c147c1f2c
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-03-06 14:53:25 +0100

    [#22] Inicijalizovana komponenta za dodavanje projekata

--- app/Client/src/app/app.component.ts
+++ app/Client/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { AddMemberComponent } from "./components/add-member/add-member.component";
 import { AllProjectsComponentComponent } from "./components/all-projects-component/all-projects-component.component";
 import { AllMembersComponentComponent } from "./components/all-members-component/all-members-component.component";
+import { ProjectAddComponent } from './components/project-add/project-add.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, AllProjectsComponentComponent, AllMembersComponentComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, AllProjectsComponentComponent, AllMembersComponentComponent, ProjectAddComponent]
 })
 export class AppComponent {
   title = 'Client';
