22d244159f169b540fd5fdf4b4f93e8e80446082
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-07 14:23:37 +0100

    Zavrsena osnova funkcionalnost navbara

--- app/Client/src/app/components/navbar/navbar.component.ts
+++ app/Client/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
