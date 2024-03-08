e230bd1255af21a2140876eb2c6c409e80f7b180
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-03-08 23:03:47 +0100

    #32 Promenjen je sam prikaz projekata u grid view

--- app/Client/src/app/components/all-projects/all-projects.component.ts
+++ app/Client/src/app/components/all-projects/all-projects.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProjectComponent } from "../../models/project/project.component";
+import { Project } from '../../project';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-all-projects',
     standalone: true,
     templateUrl: './all-projects.component.html',
     styleUrl: './all-projects.component.scss',
-    imports: [ProjectComponent]
+    imports: [ProjectComponent, CommonModule]
 })
-export class AllProjectsComponent {
+export class AllProjectsComponent implements OnInit{
+    
+
+    projects : Project[] = [];
+    project : Project = {
+        Id: 1,
+        Name: 'Project 1',
+        DateStarts: 123,
+        DateEnds: 321,
+        Details: 'Details1 ',
+        Status: 'Active',
+        Lead: 'Pera'
+    };
+
+    ngOnInit(): void {
+        this.projects.push(this.project);
+        this.projects.push(this.project);
+        this.projects.push(this.project);
+    }
+
+
 
 }
