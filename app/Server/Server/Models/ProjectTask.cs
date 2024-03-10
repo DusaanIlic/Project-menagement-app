5f95e32b10525b504bf6f66c5bf515be5ef912ac
aleksJankovic <saska.jankovic19@gmail.com>
2024-03-10 23:33:48 +0100

    #35 impelemntiran model task, modifikovan project entitet

---
+++ app/Server/Server/Models/ProjectTask.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
+{
+    public class ProjectTask
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskId { get; set; }
+
+        public string TaskName { get; set; } = string.Empty;
+
+        public string TaskDescription { get; set; } = string.Empty;
+
+        public DateOnly DeadLine { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
