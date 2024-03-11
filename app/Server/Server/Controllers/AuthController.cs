5485c688eb75500425b3075da47917652648a892
Kila369 <67660152+Kila369@users.noreply.github.com>
2024-03-11 14:03:15 +0100

    #[42]: Create Auth controller with login method

---
+++ app/Server/Server/Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.DataTransferObjects;
+using Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
+
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly LogicTenacityDbContext dbContext;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(LogicTenacityDbContext dbContext, IConfiguration configuration)
+        {
+            this.dbContext = dbContext;
+            this._configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        {
+            var member = await dbContext.Members.FirstOrDefaultAsync(u => u.Email == loginRequest.Email);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(loginRequest.Password, member.Password))
+            {
+                return Unauthorized();
+            }
+
+            var token = GenerateJwtToken(member);
+
+            return Ok(new { Token = token });
+        }
+
+        private string GenerateJwtToken(Member member)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, member.Email),
+                    new Claim(ClaimTypes.Role, member.Role) // Add role claim here
+                }),
+                Expires = DateTime.UtcNow.AddDays(7), // Token expiration time
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Audience = _configuration["JwtSettings:Audience"],
+                Issuer = _configuration["JwtSettings:Issuer"]
+
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
