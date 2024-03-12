7bca2e53fb62f09e628598055c533b0d8c637ba5
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-03-12 23:00:44 +0100

    #46 Dropdown komponenta je potpuno responsive i spremna je za koriscenje

--- app/Client/src/app/components/profile-dropdown-menu/profile-dropdown-menu.component.ts
+++ app/Client/src/app/components/profile-dropdown-menu/profile-dropdown-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-dropdown-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './profile-dropdown-menu.component.html',
   styleUrl: './profile-dropdown-menu.component.scss'
 })
