e89b3266766645ec070868b2c456614565ef69f2
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-03-15 17:01:58 +0100

    #54 Dodat je funkcionalan search bar za AllMembers

--- app/Client/src/app/components/all-members/all-members.component.ts
+++ app/Client/src/app/components/all-members/all-members.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Member } from '../../models/member';
 import {RouterLink} from "@angular/router";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-all-members',
     standalone: true,
     templateUrl: './all-members.component.html',
     styleUrl: './all-members.component.scss',
-    imports: [CommonModule, RouterLink]
+    imports: [CommonModule, RouterLink, FormsModule]
 })
 export class AllMembersComponent implements OnInit{
 
-
     members : Member[] = [];
     member1? : Member;
-
-    addNewMember()
-    {
-        console.log("Add memeber");
-    }
 
     ngOnInit(): void {
 
@@ -31,9 +27,45 @@ export class AllMembersComponent implements OnInit{
             dateAdded : '2024-03-13',
           };
 
+          this.members?.push(this.member1)
+
+          this.member1 = {
+            id: 1,
+            fullName: 'Mika Mikic',
+            email: 'peraperic@gmail.com',
+            role: 'Project Menager',
+            dateAdded : '2024-03-13',
+          };
+
+          this.members?.push(this.member1)
+
+          this.member1 = {
+            id: 1,
+            fullName: 'Laza Lazic',
+            email: 'peraperic@gmail.com',
+            role: 'Project Menager',
+            dateAdded : '2024-03-13',
+          };
+
         this.members?.push(this.member1)
         this.members?.push(this.member1)
         this.members?.push(this.member1)
         this.members?.push(this.member1)
     }
+
+    filteredMembers: Member[] = [];
+    searchQuery: string = '';
+
+    constructor() {
+        this.filteredMembers = this.members;
+    }
+
+  search(): void {
+    if (!this.searchQuery.trim()) {
+      this.filteredMembers = this.members;
+      return;
+    }
+    const regex = new RegExp(this.searchQuery.trim(), 'i');
+    this.filteredMembers = this.members.filter(member => regex.test(member.fullName));
+  }
 }
