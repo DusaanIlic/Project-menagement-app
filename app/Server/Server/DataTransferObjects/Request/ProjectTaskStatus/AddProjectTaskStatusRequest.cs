1ff5895eeed61ce8c57e797e131a8e90f3fca463
aleksJankovic <saska.jankovic19@gmail.com>
2024-03-17 17:34:50 +0100

    #50 dodat kontroler sa crud operacijama nad entitom project task status

---
+++ app/Server/Server/DataTransferObjects/Request/ProjectTaskStatus/AddProjectTaskStatusRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.DataTransferObjects.Request.ProjectTaskStatus
+{
+    public class AddProjectTaskStatusRequest
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
