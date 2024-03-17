5256335b0ddb26b3ca9eeed1f20f727a1e47be98
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-17 20:13:27 +0100

    [#56] Inputi sredjeni da se smanjuju

--- app/Client/src/app/forms/add-member.form.ts
+++ app/Client/src/app/forms/add-member.form.ts
@@ -1,7 +1,11 @@
 export interface AddMemberForm {
   id: string;
+  firstName: string;
+  lastName: string;
   fullName: string;
   email: string;
+  phoneNumber: string;
+  dateOfBirth: string;
   role: string;
   password: string;
 }
