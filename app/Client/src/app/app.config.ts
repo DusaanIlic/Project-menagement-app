91dd5c605258728e5886302ac8ad1de07ae2aaae
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-21 19:47:34 +0100

    [#68] Dodat angular material u package.json

--- app/Client/src/app/app.config.ts
+++ app/Client/src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {HttpClientModule, provideHttpClient, withInterceptors} from '@angular/common/http';
 import {httpInterceptor} from "./helpers/http.interceptor";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideHttpClient(withInterceptors([httpInterceptor]))]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideHttpClient(withInterceptors([httpInterceptor])), provideAnimationsAsync()]
 };
