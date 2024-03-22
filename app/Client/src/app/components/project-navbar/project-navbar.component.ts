634857099fbfe3720e47447ec9b7418b967d7656
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-23 00:28:24 +0100

    [#70] Inicijalizovana project navbar komponenta

---
+++ app/Client/src/app/components/project-navbar/project-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-navbar.component.html',
+  styleUrl: './project-navbar.component.scss'
+})
+export class ProjectNavbarComponent {
+
+}
