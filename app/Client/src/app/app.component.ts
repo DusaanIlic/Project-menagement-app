7e024507b966dc4f4214b42c47bc67451ab68193
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-23 00:47:41 +0100

    [#70] Project navbar se sada pokazuje samo kada url sadrzi /project/ u sebi

--- app/Client/src/app/app.component.ts
+++ app/Client/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { AddProjectComponent } from './components/add-project/add-project.compon
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {NgIf} from "@angular/common";
 import { initFlowbite } from 'flowbite';
+import {ProjectNavbarComponent} from "./components/project-navbar/project-navbar.component";
 
 @Component({
   selector: 'app-root',
@@ -20,26 +21,18 @@ import { initFlowbite } from 'flowbite';
     AddProjectComponent,
     NavbarComponent,
     NgIf,
+    ProjectNavbarComponent,
   ],
 })
 export class AppComponent implements OnInit{
   showNavbar: boolean = true;
-  showHeader: boolean = true;
-  pageTitle: string = 'Default Title';
+  showProjectNavbar: boolean = false;
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
-        if (event.url === '/login') {
-          this.showNavbar = false;
-          this.showHeader = false;
-        }
-      }
-      if (event instanceof NavigationEnd) {
-        if (event.url !== '/login') {
-          this.showNavbar = true;
-          this.showHeader = true;
-        }
+        this.showNavbar = event.url !== '/login';
+        this.showProjectNavbar = event.url.includes('/project/')
       }
     });
   }
