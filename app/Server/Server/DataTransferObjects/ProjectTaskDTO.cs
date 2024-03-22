e8024a3a0662f65d36fd049d7e6f072347837d75
aleksJankovic <saska.jankovic19@gmail.com>
2024-03-22 23:30:33 +0100

    #67 napravljen task priority controller

--- app/Server/Server/DataTransferObjects/ProjectTaskDTO.cs
+++ app/Server/Server/DataTransferObjects/ProjectTaskDTO.cs
@@ -5,9 +5,11 @@
         public int TaskId { get; set; }
         public string TaskName { get; set; }
         public string TaskDescription { get; set; }
+        public int StartDate { get; set; }
         public DateTime Deadline { get; set; }
         public int ProjectId { get; set; }
         public string TaskStatus { get; set; }
         public int TaskStatusId { get; set; }
+        public int TaskPriorityId { get; set; }
     }
 }
