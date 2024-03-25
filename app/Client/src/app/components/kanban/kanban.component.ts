be220cbfb76083758a89483d6af80f91a81617f9
Dusan Ilic <ilicdusan2002@gmail.com>
2024-03-25 19:05:18 +0100

    #69, Kreiran task service (osnova)

--- app/Client/src/app/components/kanban/kanban.component.ts
+++ app/Client/src/app/components/kanban/kanban.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList, CdkDropListGroup} from '@angular/cdk/drag-drop';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-kanban',
   standalone: true,
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, CdkDropListGroup, NgFor],
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss'
 })
 
 export class KanbanComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  todo = ['Figma prototip', 'Gantt', 'Kanban', 'Members'];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  progress = ['Tabela', 'Projekti', 'Taskovi', 'Analitika'];
+
+  done = ['Dashboard', 'Login', 'Register', 'UserProfil'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -24,6 +27,16 @@ export class KanbanComponent {
         event.previousIndex,
         event.currentIndex,
       );
+    }
+  }
+
+  deleteTask(column: string, index: number) {
+    if (column === 'todo') {
+      this.todo.splice(index, 1);
+    } else if (column === 'progress') {
+      this.progress.splice(index, 1);
+    } else if (column === 'done') {
+      this.done.splice(index, 1);
     }
   }
 }
