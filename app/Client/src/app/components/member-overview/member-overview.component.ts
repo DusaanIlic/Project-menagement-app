b5c990d60b2d3747042ff47b15b832278348518b
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-03-25 19:44:09 +0100

    #73 Napravljen je bolji prikaz activity-ja i dodat je odgovarajuci interfejs

--- app/Client/src/app/components/member-overview/member-overview.component.ts
+++ app/Client/src/app/components/member-overview/member-overview.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Member } from '../../models/member';
+import { taskActivity } from '../../models/taskActivity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-member-overview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './member-overview.component.html',
   styleUrl: './member-overview.component.scss'
 })
-export class MemberOverviewComponent {
-
+export class MemberOverviewComponent implements OnInit{
+  
   member : Member = {
     id: 1,
     fullName: 'Pera Peric',
@@ -17,4 +19,25 @@ export class MemberOverviewComponent {
     role: 'Project Manager',
     dateAdded: '1/1/2024'
   };
+
+  activity : taskActivity = {
+    projectName : "Project 123",
+    taskName : "Task 1",
+    memberName : "Pera Peric",
+    type : "Update",
+    dateModified : Date.now(),
+    description : "I made gantt view without colours."
+  }
+  activities : taskActivity[] = [];
+
+
+
+  ngOnInit(): void {
+    this.activities.push(this.activity);
+    this.activities.push(this.activity);
+    this.activities.push(this.activity);
+    this.activities.push(this.activity);
+    this.activities.push(this.activity);
+  }
+
 }
---
+++ app/Client/src/app/models/taskActivity.ts
@@ -0,0 +1,9 @@
+export interface taskActivity
+{
+    projectName? : string,
+    taskName? : string,
+    memberName? : string,
+    type? : string,
+    dateModified? : number,
+    description? : string
+}
