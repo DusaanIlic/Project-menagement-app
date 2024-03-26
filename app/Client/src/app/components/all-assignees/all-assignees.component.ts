ced933bb288624d3f58e94d27df3214e8ff6ab89
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-26 23:57:30 +0100

    [#77] Inicijalizovana komponenta

---
+++ app/Client/src/app/components/all-assignees/all-assignees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-assignees',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-assignees.component.html',
+  styleUrl: './all-assignees.component.scss'
+})
+export class AllAssigneesComponent {
+
+}
