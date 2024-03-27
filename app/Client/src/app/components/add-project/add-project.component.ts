6cf64caff39efbae0e84a9f48d2158098ff7a85a
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-03-28 00:07:00 +0100

    [#55] ispis uspesnosti dodavanja projekta

--- app/Client/src/app/components/add-project/add-project.component.ts
+++ app/Client/src/app/components/add-project/add-project.component.ts
@@ -7,15 +7,19 @@ import {
 } from '@angular/forms';
 import { ProjectService } from '../../services/add.project.service';
 import { ProjectAddRequest } from '../../models/project-add';
+import { tap } from 'rxjs/internal/operators/tap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-project',
   standalone: true,
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.scss'],
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
 })
 export class AddProjectComponent {
+  isSuccess: boolean = false;
+  isError: boolean = false;
   projectForm = new FormGroup({
     projectName: new FormControl('', [Validators.required]),
     deadLine: new FormControl('', [Validators.required]),
@@ -29,17 +33,39 @@ export class AddProjectComponent {
   public onSubmit() {
     if (this.projectForm.valid) {
       const formValue = this.projectForm.value;
-      // Convert string value to Date object for deadLine
       const sanitizedFormValue: ProjectAddRequest = {
         projectName: formValue.projectName || '',
-        // Ensure deadLine is a Date object
         deadLine: formValue.deadLine ? new Date(formValue.deadLine) : undefined,
         projectDescription: formValue.projectDescription || '',
       };
 
-      // Obrada podataka
-      console.log(sanitizedFormValue);
-      this.projectService.createProject(sanitizedFormValue);
+      this.projectService
+        .createProject(sanitizedFormValue)
+        .pipe(
+          tap({
+            next: (response) => {
+              // Ako je projekat uspešno dodat
+              console.log('Projekat je uspešno dodat', response);
+              // poruka na korisničkom interfejsu
+              this.showSuccessMessage();
+            },
+            error: (error) => {
+              // Ako se dogodi greška prilikom dodavanja projekta
+              console.error('Greška prilikom dodavanja projekta:', error);
+              // poruka o grešci na korisničkom interfejsu
+              this.showErrorMessage();
+            },
+          })
+        )
+        .subscribe();
     }
+  }
+  showErrorMessage() {
+    this.isError = true;
+    this.isSuccess = false;
+  }
+  showSuccessMessage() {
+    this.isSuccess = true;
+    this.isError = false;
   }
 }
