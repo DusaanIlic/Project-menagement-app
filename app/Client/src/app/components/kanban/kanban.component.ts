be7c1ee9d26a0a632f431282ca8e47930908650f
Dusan Ilic <ilicdusan2002@gmail.com>
2024-03-27 23:46:43 +0100

    #75, Dodata potvrda o obrisanom task-u

--- app/Client/src/app/components/kanban/kanban.component.ts
+++ app/Client/src/app/components/kanban/kanban.component.ts
@@ -6,13 +6,15 @@ import { throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { FormsModule, NgModel } from '@angular/forms';
 import { ChangeDetectorRef } from '@angular/core';
+import { NgToastModule, NgToastService } from 'ng-angular-popup';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-kanban',
   standalone: true,
-  imports: [CdkDropList, CdkDrag, CdkDropListGroup, NgFor, FormsModule, CommonModule],
-  templateUrl: './kanban.component.html',
-  styleUrl: './kanban.component.scss'
+  imports: [CdkDropList, CdkDrag, CdkDropListGroup, NgFor, FormsModule, CommonModule, NgToastModule, MatDialogModule],
+  templateUrl: './kanban.component.html', 
+  styleUrl: './kanban.component.scss',
 })
 
 export class KanbanComponent {
@@ -37,7 +39,7 @@ export class KanbanComponent {
     this.showDoneList = !this.showDoneList;
   }
 
-  constructor(private taskService: TaskService, private cdr: ChangeDetectorRef) {}
+  constructor(private taskService: TaskService, private cdr: ChangeDetectorRef,  private _ngToastService: NgToastService, private dialog: MatDialog) {}
 
   ngOnInit(): void{
     this.loadTasksByProject(1);
@@ -59,7 +61,6 @@ export class KanbanComponent {
       )
       .subscribe();
   }
-
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -84,26 +85,52 @@ export class KanbanComponent {
     }
 }
 
+  findTaskIndex(taskId: number, column: string): number {
+      let taskList: any[];
+
+      switch (column) {
+        case 'todo':
+          taskList = this.todo;
+          break;
+        case 'progress':
+          taskList = this.progress;
+          break;
+        case 'done':
+          taskList = this.done;
+          break;
+        default:
+          return -1; 
+      }
+
+      const taskIndex = taskList.findIndex(task => task.taskId === taskId);
+      return taskIndex;
+  }
+
   deleteTask(column: string, index: number) {    
     if (column === 'todo') {
-      this.todo.splice(index, 1);
+      this.todo.splice(this.findTaskIndex(index, column), 1);
+      this.showMessage();
       this.taskService.deleteTask(index).subscribe(
         (error) => console.log("error", error)
       );
     } else if (column === 'progress') {
-      this.progress.splice(index, 1);
+      this.progress.splice(this.findTaskIndex(index, column), 1);
+      this.showMessage();
       this.taskService.deleteTask(index).subscribe(
         (error) => console.log("error", error)
       );
     } else if (column === 'done') {
-      this.done.splice(index, 1);
+      this.done.splice(this.findTaskIndex(index, column), 1);
+      this.showMessage();
       this.taskService.deleteTask(index).subscribe(
         (error) => console.log("error", error)
       );
     }
   }
 
-  printProgress(){
-    console.log(this.progress);
+  showMessage(){
+    this._ngToastService.success({detail: "Success Message", summary: "Deleted successfully", duration: 3000});
   }
+
+
 }
