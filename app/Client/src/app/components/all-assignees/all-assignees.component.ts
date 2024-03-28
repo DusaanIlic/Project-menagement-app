e121eb32096154f270c1855c086a51fb754a482f
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-28 14:24:07 +0100

    [#77] Zavrsen user row

--- app/Client/src/app/components/all-assignees/all-assignees.component.ts
+++ app/Client/src/app/components/all-assignees/all-assignees.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-all-assignees',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './all-assignees.component.html',
   styleUrl: './all-assignees.component.scss'
 })
