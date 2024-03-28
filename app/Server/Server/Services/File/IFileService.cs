e3ecebbbf4668180065562907bebbf93ec0475de
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-28 20:11:46 +0100

    [#80] Dodat fajl servis interfejs

--- app/Server/Server/Services/File/IFileService.cs
+++ app/Server/Server/Services/File/IFileService.cs
@@ -1,9 +1,13 @@
-﻿namespace Server.Services.File
+﻿using Server.Models;
+
+namespace Server.Services.File
 {
     public interface IFileService
     {
         public Task PostFileAsync(IFormFile fileData);
 
-        public Task PostMultiFileAsync(List<Models.UploadedFile> fileData);
+        public Task PostMultiFileAsync(List<UploadedFile> fileData);
+
+        public Task DownloadFileById(int fileId);
     }
 }
