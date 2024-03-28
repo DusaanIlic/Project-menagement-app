235b07cc66c4589523d54b9bcfce6c1656c5e646
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-28 19:36:54 +0100

    [#80] Dodat fajl entitet

---
+++ app/Server/Server/Models/File.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models
+{
+    public class File
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FileId { get; set; }
+        
+        [Required(ErrorMessage = "File path is required.")]
+        public string FilePath { get; set; }
+    }
+}
