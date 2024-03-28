64298bba8ff04f77fc9f87333bba42a0cf020002
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-03-28 20:21:52 +0100

    [#80] Dodat kontroler

---
+++ app/Server/Server/Controllers/UploadedFileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadedFileController
+    {
+        
+    }
+}
+
--- app/Server/Server/Services/File/FileService.cs
+++ app/Server/Server/Services/File/FileService.cs
@@ -1,8 +1,34 @@
-﻿namespace Server.Services.File
+﻿using System.Drawing;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Services.File
 {
-    public class FileService
+    public class FileService : IFileService
     {
-    
+        private readonly LogicTenacityDbContext _dbContextClass;
+        private readonly IConfiguration _configuration;
+
+        public FileService(LogicTenacityDbContext dbContextClass, IConfiguration configuration)
+        {
+            this._dbContextClass = dbContextClass;
+            this._configuration = configuration;
+        }
+        
+        public Task PostFileAsync(IFormFile fileData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostMultiFileAsync(List<UploadedFile> fileData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DownloadFileById(int fileId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
