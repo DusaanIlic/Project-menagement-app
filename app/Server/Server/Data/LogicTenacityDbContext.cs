ef745b4b0c4068f7117c5d027ac4a85bceb9be55
aleksJankovic <saska.jankovic19@gmail.com>
2024-03-29 21:10:06 +0100

    #53 dodata veza izmedju rola i permisija, uradjena migracija

--- app/Server/Server/Data/LogicTenacityDbContext.cs
+++ app/Server/Server/Data/LogicTenacityDbContext.cs
@@ -22,6 +22,7 @@ namespace Server.Data
         public DbSet<File> Files { get; set; }
         public DbSet<Role> Roles {  get; set; }
         public DbSet<Permission> Permissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -98,6 +99,19 @@ namespace Server.Data
                .HasOne(m => m.Role)
                .WithMany(r => r.Members)
                .HasForeignKey(m => m.RoleId);
+
+            modelBuilder.Entity<RolePermission>()
+            .HasKey(rp => new { rp.RoleId, rp.PermissionId });
+
+            modelBuilder.Entity<Role>()
+                .HasMany(r => r.RolePermissions)
+                .WithOne(rp => rp.Role)
+                .HasForeignKey(rp => rp.RoleId);
+
+            modelBuilder.Entity<Permission>()
+                .HasMany(p => p.RolePermissions)
+                .WithOne(rp => rp.Permission)
+                .HasForeignKey(rp => rp.PermissionId);
         }
     }
 }
--- app/Server/Server/Models/Permission.cs
+++ app/Server/Server/Models/Permission.cs
@@ -4,5 +4,7 @@
     {
         public int PermissionId { get; set; }
         public string PermissionName { get; set; }
+
+        public ICollection<RolePermission> RolePermissions { get; set; }
     }
 }
