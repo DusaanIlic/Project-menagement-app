6dda91eb342abfc9b05ebfc0f4e95e89e84edd1f
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-03-30 01:12:55 +0100

    [#81] izmenjen main.ts tako da podrzava MatDialogModule

--- app/Client/src/main.ts
+++ app/Client/src/main.ts
@@ -1,6 +1,17 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { provideRouter, withDebugTracing } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app/app.routes';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withDebugTracing()),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(MatDialogModule),
+    provideAnimations(),
+  ],
+}).catch((err: any) => console.error(err));
