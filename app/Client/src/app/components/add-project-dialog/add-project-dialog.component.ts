1e4a686dcf0f0c800576b135ac2c57326accc39f
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-04-01 16:31:38 +0200

    [#81] kreirana nova add-project-dialog komponenta

---
+++ app/Client/src/app/components/add-project-dialog/add-project-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-project-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-project-dialog.component.html',
+  styleUrl: './add-project-dialog.component.scss'
+})
+export class AddProjectDialogComponent {
+
+}
