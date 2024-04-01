72bbfd0f0943e57a2f66d6380b2d4762b3c5527e
aleksJankovic <saska.jankovic19@gmail.com>
2024-04-01 21:39:50 +0200

    #53 dodate pocetne permisije

---
+++ app/Server/Server/Migrations/20240401192540_InitPermission.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitPermission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+              migrationBuilder.InsertData(
+              table: "Permissions",
+              columns: new[] { "PermissionId", "PermissionName" },
+              values: new object[,]
+              {
+                    { 1, "Add members" },
+                    { 2, "Deactivate members" },
+                    { 3, "Create project" },
+                    { 4, "Create task" },
+                    { 5, "Delete project" },
+                    { 6, "Delete task" },
+                    { 7, "Add member to task"},
+                    { 8, "Add member to project"},
+                    { 9, "Remove member from task"},
+                    { 10, "Remove member from project"},
+                    { 11, "Change task status"},
+                    { 12, "Change project status" }
+              });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+       table: "Permissions",
+       keyColumn: "PermissionId",
+       keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "PermissionId",
+                keyValue: 12);
+
+        }
+    }
+}
