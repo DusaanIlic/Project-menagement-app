511170ae92888c57a5fbdb70010aa26835cf923e
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-02 01:38:55 +0200

    [#82] Dodat limit od 512KB na slike za sad

--- app/Server/Server/DataTransferObjects/Request/File/AddFileRequest.cs
+++ app/Server/Server/DataTransferObjects/Request/File/AddFileRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Server.Attributes;
 
 namespace Server.DataTransferObjects.Request.File;
 
@@ -6,5 +7,6 @@ public class AddFileRequest
 {
     [Required]
     [DataType(DataType.Upload)]
+    [MaxFileSize(512 * 1024)] // 512KB
     public IFormFile FileDetails { get; set; }
 }
