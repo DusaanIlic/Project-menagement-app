9cf1e594b50520cb087984120a476d8f982452ed
Kila369 <67660152+Kila369@users.noreply.github.com>
2024-04-04 10:52:08 +0200

    [#72]: Create migrations for task comment entity

---
+++ app/Server/Server/Migrations/20240403185307_AddTaskComments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MemberId",
+                table: "TaskComments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MemberId",
+                table: "TaskComments");
+        }
+    }
+}
