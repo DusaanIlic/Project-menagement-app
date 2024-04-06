b8f29894c5fbb061bad5127c30fc5a481c207840
aleksJankovic <saska.jankovic19@gmail.com>
2024-04-06 21:55:55 +0200

    #87 dodat servis za proveru permisija

---
+++ app/Server/Server/Services/RolePermission/IRolePermissionService.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Services.RolePermission
+{
+    public interface IRolePermissionService
+    {
+        Task<bool> CheckRolePermission(int roleId, int permissionId);
+    }
+}
---
+++ app/Server/Server/Services/RolePermission/RolePermissionService.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
+namespace Server.Services.RolePermission
+{
+    public class RolePermissionService : IRolePermissionService
+    {
+        private readonly LogicTenacityDbContext dbContext;
+
+        public RolePermissionService(LogicTenacityDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<bool> CheckRolePermission(int roleId, int permissionId)
+        {
+            return await dbContext.RolePermissions
+                .AnyAsync(rp => rp.RoleId == roleId && rp.PermissionId == permissionId);
+        }
+    }
+}
