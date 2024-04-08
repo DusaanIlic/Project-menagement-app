f5d0ae3a7abd07decbfb0c8c192b08dbe5220a17
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-08 19:52:33 +0200

    [#94] Uradjen middleware

---
+++ app/Server/Server/Middlewares/JwtBlacklistMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
+namespace Server.Middlewares;
+
+public class JwtBlacklistMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly LogicTenacityDbContext _dbContext;
+
+    public JwtBlacklistMiddleware(RequestDelegate next, LogicTenacityDbContext dbContext)
+    {
+        _next = next;
+        _dbContext = dbContext;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var token = context.Request.Headers["Authorization"]
+            .FirstOrDefault()?.Split(" ").Last();
+        
+        if (!string.IsNullOrEmpty(token)) {
+            var blacklistedToken = await _dbContext.BlacklistedTokens.AnyAsync(t => t.Token == token);
+
+            if (blacklistedToken)
+            {
+                context.Response.StatusCode = 401;
+                return;
+            }
+        }
+
+        await _next(context);
+    }
+}
