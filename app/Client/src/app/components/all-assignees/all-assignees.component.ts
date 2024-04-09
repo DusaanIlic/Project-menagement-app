50402ef97225e1e8e1ee75552bb918773bd9dd13
Veljko Djurovic <41-2021@pmf.kg.ac.rs>
2024-04-09 23:58:01 +0200

    #97 Inicijalizovan pocetni izgled tabele

--- app/Client/src/app/components/all-assignees/all-assignees.component.ts
+++ app/Client/src/app/components/all-assignees/all-assignees.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {DatePipe, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-all-assignees',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    DatePipe,
+    MatButton,
+    MatMenu,
+    MatMenuItem,
+    NgForOf,
+    NgIf,
+    MatMenuTrigger,
+    RouterLink
   ],
   templateUrl: './all-assignees.component.html',
   styleUrl: './all-assignees.component.scss'
 })
-export class AllAssigneesComponent {
+export class AllAssigneesComponent implements OnInit{
+  private routeSub: any;
+  assignes : any[] = [1];
 
+  clickMethod()
+  {
+  }
+
+
+
+  constructor(private route: ActivatedRoute) { }
+  ngOnInit(): void
+  {
+    this.routeSub = this.route.params.subscribe((params : any) => {
+      console.log(params['id']);
+    })
+  }
 }
