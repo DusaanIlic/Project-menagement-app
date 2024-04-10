7b5ef08f21190eb18477606be998d2d70ec129b5
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-04-11 01:11:28 +0200

    [#71] izmenjen gant

--- app/Client/src/app/components/gantogram/gantogram.component.ts
+++ app/Client/src/app/components/gantogram/gantogram.component.ts
@@ -1,39 +1,50 @@
-import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  GanttChartComponent,
-  GanttChartModule,
-  GanttChartTaskColumn,
-} from 'smart-webcomponents-angular/ganttchart';
+import { GanttModule } from '@syncfusion/ej2-angular-gantt';
 
 @Component({
   standalone: true,
   selector: 'app-gantogram',
-  imports: [GanttChartModule, ReactiveFormsModule],
+  imports: [GanttModule, ReactiveFormsModule],
   templateUrl: './gantogram.component.html',
   styleUrls: ['./gantogram.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class GantComponent {
-  title = 'smart-app';
-
-  @ViewChild('gantt', { read: GanttChartComponent, static: false })
-  gantt!: GanttChartComponent;
-
-  durationUnit = 'hour';
-
-  taskColumns: GanttChartTaskColumn[] = [
-    {
-      label: 'Tasks',
-      value: 'label',
-      size: '60%',
-    },
-    {
-      label: 'Duration (hours)',
-      value: 'duration',
-      formatFunction: (date: string) => parseInt(date),
-    },
-  ];
-
-  dataSource = [];
+export class GantComponent implements OnInit {
+  public data: object[] | undefined;
+  public taskSettings: object | undefined;
+  public columns: object[] | undefined;
+  public labelSettings: object | undefined;
+  public projectStartDate: Date | undefined;
+  public projectEndDate: Date | undefined;
+  public ngOnInit(): void {
+    this.taskSettings = {
+      id: 'TaskID',
+      name: 'TaskName',
+      startDate: 'StartDate',
+      endDate: 'EndDate',
+      duration: 'Duration',
+      progress: 'Progress',
+      dependency: 'Predecessor',
+      child: 'subtasks',
+    };
+    this.columns = [
+      { field: 'TaskID', width: 80 },
+      {
+        field: 'TaskName',
+        headerText: 'Job Name',
+        width: '250',
+        clipMode: 'EllipsisWithTooltip',
+      },
+      { field: 'StartDate' },
+      { field: 'Duration' },
+      { field: 'Progress' },
+      { field: 'Predecessor' },
+    ];
+    this.projectStartDate = new Date('03/24/2019');
+    this.projectEndDate = new Date('07/06/2019');
+    this.labelSettings = {
+      leftLabel: 'TaskName',
+    };
+  }
 }
