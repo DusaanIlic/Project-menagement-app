853d4a4a54b61fcccab44ae4dc389a0ae58125c6
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-10 04:07:07 +0200

    [#94] Popravljen i glavni navbar

--- app/Client/src/app/app.routes.ts
+++ app/Client/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes } from '@angular/router';
+import {RouterConfigOptions, RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { AllMembersComponent } from "./components/all-members/all-members.component";
 import { AllProjectsComponent } from "./components/all-projects/all-projects.component";
@@ -16,16 +16,20 @@ import { MemberOverviewComponent } from './components/member-overview/member-ove
 import {AllAssigneesComponent} from "./components/all-assignees/all-assignees.component";
 import {ProjectComponent} from "./components/project/project.component";
 
+export const routerConfig: RouterConfigOptions = {
+  paramsInheritanceStrategy: 'always'
+};
+
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'members/all', component: AllMembersComponent, canActivate: [AuthGuard] },
-  { path: 'members/edit/:id', component: EditMemberComponent, canActivate: [AuthGuard]}, //Umeso 1 treba da bude ID membera koji se edituje
+  { path: 'members/:id/edit', component: EditMemberComponent, canActivate: [AuthGuard]}, //Umeso 1 treba da bude ID membera koji se edituje
   { path: 'projects/all', component: AllProjectsComponent, canActivate: [AuthGuard] },
   { path: 'projects/add', component: AddProjectComponent, canActivate: [AuthGuard]  },
   { path: 'roles/all', component: AllRolesComponent, canActivate: [AuthGuard] },
-  { path: 'roles/edit/:id', component: EditRoleComponent, canActivate: [AuthGuard] },
+  { path: 'roles/:id/edit', component: EditRoleComponent, canActivate: [AuthGuard] },
   { path: 'members/:id', component: MemberOverviewComponent, canActivate: [AuthGuard] },
   {
     path: 'projects/:id', // The part causing me a headache
