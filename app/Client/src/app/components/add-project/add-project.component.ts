dccac30d23232a4aac27ca4f4b68e67e60a448a0
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-04-10 19:14:14 +0200

    [#81] poruka o uspesnom/neuspesnom dodavanju projekta

--- app/Client/src/app/components/add-project/add-project.component.ts
+++ app/Client/src/app/components/add-project/add-project.component.ts
@@ -23,13 +23,14 @@ import { NgxEditorModule } from 'ngx-editor';
 import { Editor } from 'ngx-editor';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectAddRequest } from '../../models/project-add';
+import { NgToastModule, NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-add-project',
   standalone: true,
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.scss'],
-  imports: [FormsModule, CommonModule, NgxEditorModule],
+  imports: [FormsModule, CommonModule, NgxEditorModule, NgToastModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AddProjectComponent implements OnDestroy {
@@ -49,7 +50,8 @@ export class AddProjectComponent implements OnDestroy {
   constructor(
     public dialogRef: MatDialogRef<AddProjectComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private _ngToastService: NgToastService
   ) {}
 
   ngOnDestroy(): void {
@@ -60,22 +62,60 @@ export class AddProjectComponent implements OnDestroy {
     this.dialogRef.close();
   }
 
+  showMessage() {
+    this._ngToastService.success({
+      detail: 'Success Message',
+      summary: 'Project added successfully!',
+      duration: 3000,
+    });
+  }
+
+  showMessageError() {
+    this._ngToastService.error({
+      detail: 'Error Message',
+      summary: 'Project add failed!',
+      duration: 3000,
+    });
+  }
+
   public onSubmit() {
-    this.projectForm = new FormGroup({
+    /*this.projectForm = new FormGroup({
       projectName: new FormControl(this.projectName, Validators.required),
       projectDescription: new FormControl(this.html, Validators.required),
       deadLine: new FormControl(this.deadLine, Validators.required),
-    });
-    const projectData: ProjectAddRequest = this.projectForm.value;
+    });*/
+    //const projectData: ProjectAddRequest = this.projectForm.value;
+
+    const projectData = {
+      projectName: this.projectName,
+      projectDescription: this.html.trim(),
+      deadLine: this.deadLine,
+    };
+    if (!this.projectName) {
+      // Provera da li je uneti tekst prazan
+      alert('Morate uneti naziv projeta!');
+      return;
+    }
+    if (!this.html.trim()) {
+      // Provera da li je uneti tekst prazan
+      alert('Morate uneti opis projeta!');
+      return;
+    }
+    if (!this.deadLine) {
+      // Provera da li je uneti tekst prazan
+      alert('Morate uneti datum!');
+      return;
+    }
 
     this.projectService.createProject(projectData).subscribe(
       (response) => {
-        console.log('Project saved successfully:', response);
+        //console.log('Project saved successfully:', response);
         this.projectAdded.emit();
-        this.closeDialog();
+        this.showMessage();
       },
       (error) => {
-        console.error('Error saving project', error);
+        this.showMessageError();
+        //console.error('Error saving project', error);
       }
     );
   }
