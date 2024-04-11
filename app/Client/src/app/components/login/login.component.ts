0bfa07fba3204a232f5d2f062b1279fbe95d5a19
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-11 12:44:29 +0200

    [#99] Mala izmena na login komponenti

--- app/Client/src/app/components/login/login.component.ts
+++ app/Client/src/app/components/login/login.component.ts
@@ -6,6 +6,7 @@ import {Member} from "../../models/member";
 import {Router} from "@angular/router";
 import {MemberService} from "../../services/member.service";
 import {NgToastModule, NgToastService} from "ng-angular-popup";
+import {ForgotPasswordForm} from "../../forms/forgot-password.form";
 
 @Component({
   selector: 'app-login',
@@ -26,13 +27,13 @@ export class LoginComponent implements OnInit {
   password: string | undefined;
   showForgotPasswordForm: boolean = false;
 
-  forgotPasswordForm: any;
+  forgotForm: any;
 
   constructor(private authService : AuthService, private memberService: MemberService,
                 private router: Router, private _ngToastService: NgToastService) {}
 
   ngOnInit() {
-    this.forgotPasswordForm = new FormGroup({
+    this.forgotForm = new FormGroup({
       email: new FormControl('', [
         Validators.required,
         Validators.email
@@ -60,8 +61,25 @@ export class LoginComponent implements OnInit {
   }
 
   forgotPasswordSubmit() {
-    if (this.forgotPasswordForm.valid) {
+    if (this.forgotForm.valid) {
+      const data: ForgotPasswordForm = this.forgotForm.value;
 
+      this.authService.forgotPasswordRequest(data).subscribe({
+        next: data => {
+          this._ngToastService.error({
+            detail: 'Successfully sent request',
+            summary: 'Check your email for instructions',
+            duration: 2000,
+          });
+        },
+        error: error => {
+          this._ngToastService.error({
+            detail: 'Error sending request',
+            summary: error,
+            duration: 2000,
+          });
+        }
+      });
     } else {
       this._ngToastService.error({
         detail: 'Form is not valid',
