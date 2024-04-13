1e01c4bae6e3e7bb6aea06e7e91a53a970da4a13
Andjela Kidisevic <kidisevicandjela@gmail.com>
2024-04-13 23:49:30 +0200

    [#102] osnovne postavke za gantt

--- app/Client/src/app/gantt/gantt.component.ts
+++ app/Client/src/app/gantt/gantt.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GoogleChartsModule } from 'angular-google-charts';
+import { ChartType } from 'angular-google-charts';
 
 @Component({
   selector: 'app-gantt',
@@ -8,4 +9,47 @@ import { GoogleChartsModule } from 'angular-google-charts';
   templateUrl: './gantt.component.html',
   styleUrl: './gantt.component.scss',
 })
-export class GanttComponent {}
+export class GanttComponent {
+  chartData = {
+    type: ChartType.Gantt, // Koristite ChartType.Gantt umesto 'Gantt'
+    data: [
+      [
+        'Task ID',
+        'Task Name',
+        'Resource',
+        'Start Date',
+        'End Date',
+        'Duration',
+        'Percent Complete',
+        'Dependencies',
+      ],
+      [
+        '2014Spring',
+        'Spring 2014',
+        'spring',
+        new Date(2014, 2, 22),
+        new Date(2014, 5, 20),
+        null,
+        100,
+        null,
+      ],
+      // Dodajte više redova prema potrebi
+    ],
+    columnNames: [
+      'Task ID',
+      'Task Name',
+      'Resource',
+      'Start Date',
+      'End Date',
+      'Duration',
+      'Percent Complete',
+      'Dependencies',
+    ],
+    options: {
+      height: 400,
+      gantt: {
+        trackHeight: 30,
+      },
+    },
+  };
+}
