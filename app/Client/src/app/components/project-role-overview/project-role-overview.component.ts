9627538a6ff15478e83522c251817d6da55d5a96
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-17 19:13:16 +0200

    [#113] Inicijalizovana komponenta

---
+++ app/Client/src/app/components/project-role-overview/project-role-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-role-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-role-overview.component.html',
+  styleUrl: './project-role-overview.component.scss'
+})
+export class ProjectRoleOverviewComponent {
+
+}
