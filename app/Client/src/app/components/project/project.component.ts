53c1d65ed730f0d5fa50245f5b363f09ee2ee051
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-18 03:11:26 +0200

    [#115] Zavrsen sidenav

--- app/Client/src/app/components/project/project.component.ts
+++ app/Client/src/app/components/project/project.component.ts
@@ -1,17 +1,48 @@
-import { Component } from '@angular/core';
-import {ProjectNavbarComponent} from "../project-navbar/project-navbar.component";
-import {RouterOutlet} from "@angular/router";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, RouterLink, RouterOutlet} from "@angular/router";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {MatDivider} from "@angular/material/divider";
 
 @Component({
   selector: 'app-project',
   standalone: true,
   imports: [
-    ProjectNavbarComponent,
-    RouterOutlet
+    RouterOutlet,
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    NgForOf,
+    MatIcon,
+    MatSidenavContent,
+    RouterLink,
+    MatDivider,
+    NgIf
   ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
+  projectId: any;
 
+  buttons = [
+    { link: 'overview', text: 'Overview', icon: 'my_library_books' },
+    { link: 'assignees', text: 'Assigness', icon: 'assignment_ind' },
+    { link: 'tasks', text: 'Tasks', icon: 'task_alt' },
+    { link: 'gantt', text: 'Gantt', icon: 'waterfall_chart' },
+    { link: 'kanban', text: 'Kanban', icon: 'view_kanban' },
+    { link: 'analytics', text: 'Analytics', icon: 'analytics' },
+  ];
+
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.router.params.subscribe(params => {
+      this.projectId = params['id'];
+      console.log(`Project id: ${this.projectId}`);
+    })
+  }
 }
