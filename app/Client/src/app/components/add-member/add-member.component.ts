1fe2e5f3e2e17a2ddd1262c39fb9d232cc190413
Aleksa Bozinovic <aleksabozinovic25e@gmail.com>
2024-04-24 14:49:33 +0200

    [#123] Zavrsena forma za add member

--- app/Client/src/app/components/add-member/add-member.component.ts
+++ app/Client/src/app/components/add-member/add-member.component.ts
@@ -9,10 +9,30 @@ import { AddMemberForm } from '../../forms/add-member.form';
 import {MatButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {MatToolbar} from "@angular/material/toolbar";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
 @Component({
   selector: 'app-add-member',
   standalone: true,
-    imports: [FormsModule, CommonModule, NgToastModule, MatButton, MatIcon, MatToolbar],
+  imports: [
+    FormsModule,
+    CommonModule,
+    NgToastModule,
+    MatButton,
+    MatIcon,
+    MatToolbar,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    MatCardActions
+  ],
   templateUrl: './add-member.component.html',
   styleUrl: './add-member.component.scss'
 })
