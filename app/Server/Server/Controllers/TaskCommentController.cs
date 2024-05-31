29c19908a4806f6cecf00521ae450b56113887e7
aleksJankovic <saska.jankovic19@gmail.com>
2024-05-31 21:36:54 +0200

    #143 zasticeno brisanje komentara na tasku

--- app/Server/Server/Controllers/TaskCommentController.cs
+++ app/Server/Server/Controllers/TaskCommentController.cs
@@ -127,7 +127,37 @@ namespace Server.Controllers
         [HttpDelete("{taskCommentId}")]
         public async Task<IActionResult> DeleteTaskComment(int taskCommentId)
         {
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
+
+            if (userIdClaim == null)
+            {
+                return NotFound(new { message = "User ID claim not found in token" });
+            }
+
+            if (!int.TryParse(userIdClaim.Value, out var userId))
+            {
+                return BadRequest(new { message = "Invalid user ID in token" });
+            }
+
+            var member = await dbContext.Members.FindAsync(userId);
+            if (member == null)
+            {
+                return BadRequest(new { message = "Member not found" });
+            }
+
             var taskcomment = await dbContext.TaskComments.FindAsync(taskCommentId);
+
+            var projectTask = await dbContext.ProjectTasks
+               .Include(ts => ts.Project)
+               .FirstOrDefaultAsync(t => t.TaskId == taskcomment.TaskId);
+
+            var hasPermission = await _permissionService.HasProjectPermissionAsync(projectTask.ProjectId, "Delete task comment");
+            var isAssignedToTask = await _permissionService.IsMemberAssignedToTaskAsync(projectTask.TaskId);
+
+            if (!hasPermission && !isAssignedToTask)
+            {
+                return Forbid("Forbid action");
+            }
 
 
             if (taskcomment == null)
